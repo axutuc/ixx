cefcbc18243d3806fb59266ea1fc7452ccd350cc
Alfred Xutuc <165275483+axutuc@users.noreply.github.com>
2024-03-28 05:31:13 -0600

    script dev

---
+++ ixx.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "._______  _______  ___"
+echo "|   \   \/  /\   \/  /"
+echo "|   |\     /  \     /"
+echo "|   |/     \  /     \ "
+echo "|___/___/\  \/___/\  \ "
+echo "          \_/      \_/"
+echo "A hacking tools installer"
+
